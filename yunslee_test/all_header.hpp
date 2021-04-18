e7684ae1b46feb47e5c417afdf8ad674106282ec
yunslee <yunslee@student.42seoul.kr>
2021-04-18 12:53:34 +0900

    Feat: base_headers에 있는 모든 헤더파일 include하는 헤더 만듦

---
+++ yunslee_test/all_header.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../base_headers/Buffer.hpp"
+#include "../base_headers/Cgi.hpp"
+#include "../base_headers/Cgi.hpp"
+#include "../base_headers/Client.hpp"
+#include "../base_headers/Config.hpp"
+#include "../base_headers/ConfigWebserver.hpp"
+#include "../base_headers/FdSet.hpp"
+#include "../base_headers/Http.hpp"
+#include "../base_headers/Method.hpp"
+#include "../base_headers/Path.hpp"
+#include "../base_headers/Server.hpp"
+#include "../base_headers/Socket.hpp"
+#include "../base_headers/Url.hpp"
+#include "../base_headers/utils.hpp"
+#include "../base_headers/UtilsTemplate.hpp"
+#include "../base_headers/Webserver.hpp"
