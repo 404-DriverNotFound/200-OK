228a2fc8fcae567e8c1782d1e58f6d3f38438252
yunslee <yunslee@student.42seoul.kr>
2021-04-29 20:23:33 +0900

    Fix: read bug
    1. Request 내용이 모두 담기게 char buffer[]의 크기는 크게 잡고, 4000 ~ 6000
    2. gnl 처럼 버퍼에 써주고, 써준 만큼 버퍼를 내부에서 이동하고, 버퍼에 써주고 이동하고 하는 과정이 반복되야함.
    
    Resolve: #15
    Related to: #14

--- srcs/Webserver/Webserver.cpp
+++ srcs/Webserver/Webserver.cpp
@@ -12,7 +12,7 @@ void Webserver::start_server(void)
 {
 	Socket socket_one(INADDR_ANY, 8000);
 	size_t connections; // 통신 소켓
-	char buffer[BUFFER_SIZE];
+	char buffer[BUFFER_SIZE * 1000];
 	int bytesRead = BUFFER_SIZE - 1;
 
 	/* 테스트 진행 */
@@ -21,12 +21,14 @@ void Webserver::start_server(void)
 		bytesRead = BUFFER_SIZE - 1;
 		connections = socket_one.Accept(connections);
 		//	STUB : Read from the connection
+		char *temp = buffer;
 		while (bytesRead == BUFFER_SIZE - 1)
 		{
-			ft_memset(buffer, 0, BUFFER_SIZE);	//	REVIEW : 전체 탐색하는 것들은 성능 개선의 여지가 있음
-			bytesRead = read(connections, buffer, BUFFER_SIZE - 1); // request 를 여기서 받아서..
+			// ft_memset(buffer, 0, BUFFER_SIZE);	//	REVIEW : 전체 탐색하는 것들은 성능 개선의 여지가 있음
+			bytesRead = read(connections, temp, BUFFER_SIZE - 1); // request 를 여기서 받아서..
 			if (bytesRead == -1)
 				std::cerr << "Could not read request." << std::endl;
+			temp += bytesRead;
 		}
 		if (bytesRead != -1)
 		{
@@ -40,6 +42,12 @@ void Webserver::start_server(void)
 
 			//	STUB : Send a message to the connection
 			int len = response.GetMessage().size();
+
+			// FIXME 확인용. 아직 reponse header를 보내진 않았구나.
+			std::cout << "-- - - - - - - - - - - -- -  -" << std::endl;			
+			std::cout << response.GetMessage() << std::endl;
+			std::cout << "-- - - - - - - - - - - -- -  -" << std::endl;			
+			
 			int ret = send(connections, response.GetMessage().c_str(), len, 0);
 		}
 		// Close the connections
