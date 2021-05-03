f62f6010b90887627b28091137e847e3c45ca089
kohyounghwan <kohyounghwan@gmail.com>
2021-05-03 17:22:33 +0900

    Refactor: utils.hpp 파일 추가
    namespace(ft)를 이용해 커스텀함수들을 구분하기 위한 utils.hpp 파일 추가

---
+++ srcs/utils.hpp
@@ -0,0 +1,21 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <iostream>
+
+namespace ft
+{
+
+void	testNameSpace(void)
+{
+	std::cout << "namespace working" << std::endl;
+}
+
+
+
+
+
+
+
+}
+#endif
