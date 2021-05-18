c3d5db4b5045a749241630c6b77712f232f42a9c
yunslee <yunslee@student.42seoul.kr>
2021-05-19 08:31:35 +0900

    Upload: Mime type

--- srcs/Response.hpp
+++ srcs/Response.hpp
@@ -47,6 +47,7 @@ public:
 	// REVIEW const 처리를 해주는 것이 좋아보임.
 	static std::string								makeStatusPage(int status_code, std::string method);
 	static std::map<int, std::string >				m_status_map;
+	static std::map<std::string, std::string>		m_MIME_map;
 	static void				init_status_map(void)
 	{
 		m_status_map[0] = "Undefined Error";
@@ -105,6 +106,67 @@ public:
 		m_status_map[507] = "Insufficient Storage";
 		m_status_map[511] = "Network Authentication Required";
 		m_status_map[510] = "Not Extended";
+	}
+	static void init_MIME_map(void)
+	{
+		m_MIME_map[".aac"] = "audio/aac";
+		m_MIME_map[".abw"] = "application/x-abiword";
+		m_MIME_map[".arc"] = "application/octet-stream";
+		m_MIME_map[".avi"] = "video/x-msvideo";
+		m_MIME_map[".azw"] = "application/vnd.amazon.ebook";
+		m_MIME_map[".bin"] = "application/octet-stream";
+		m_MIME_map[".bz"] = "application/x-bzip";
+		m_MIME_map[".bz2"] = "application/x-bzip2";
+		m_MIME_map[".csh"] = "application/x-csh";
+		m_MIME_map[".css"] = "text/css";
+		m_MIME_map[".csv"] = "text/csv";
+		m_MIME_map[".doc"] = "application/msword";
+		m_MIME_map[".epub"] = "application/epub+zip";
+		m_MIME_map[".gif"] = "image/gif";
+		m_MIME_map[".htm"] = "text/html";
+		m_MIME_map[".html"] = "text/html";
+		m_MIME_map[".ico"] = "image/x-icon";
+		m_MIME_map[".ics"] = "text/calendar";
+		m_MIME_map[".jar"] = "Temporary Redirect";
+		m_MIME_map[".jpeg"] = "image/jpeg";
+		m_MIME_map[".jpg"] = "image/jpeg";
+		m_MIME_map[".js"] = "application/js";
+		m_MIME_map[".json"] = "application/json";
+		m_MIME_map[".mid"] = "audio/midi";
+		m_MIME_map[".midi"] = "audio/midi";
+		m_MIME_map[".mpeg"] = "video/mpeg";
+		m_MIME_map[".mpkg"] = "application/vnd.apple.installer+xml";
+		m_MIME_map[".odp"] = "application/vnd.oasis.opendocument.presentation";
+		m_MIME_map[".ods"] = "application/vnd.oasis.opendocument.spreadsheet";
+		m_MIME_map[".odt"] = "application/vnd.oasis.opendocument.text";
+		m_MIME_map[".oga"] = "audio/ogg";
+		m_MIME_map[".ogv"] = "video/ogg";
+		m_MIME_map[".ogx"] = "application/ogg";
+		m_MIME_map[".pdf"] = "application/pdf";
+		m_MIME_map[".ppt"] = "application/vnd.ms-powerpoint";
+		m_MIME_map[".rar"] = "application/x-rar-compressed";
+		m_MIME_map[".rtf"] = "application/rtf";
+		m_MIME_map[".sh"] = "application/x-sh";
+		m_MIME_map[".svg"] = "image/svg+xml";
+		m_MIME_map[".swf"] = "application/x-shockwave-flash";
+		m_MIME_map[".tar"] = "application/x-tar";
+		m_MIME_map[".tif"] = "image/tiff";
+		m_MIME_map[".tiff"] = "image/tiff";
+		m_MIME_map[".ttf"] = "application/x-font-ttf";
+		m_MIME_map[".vsd"] = " application/vnd.visio";
+		m_MIME_map[".wav"] = "audio/x-wav";
+		m_MIME_map[".weba"] = "audio/webm";
+		m_MIME_map[".webm"] = "video/webm";
+		m_MIME_map[".webp"] = "image/webp";
+		m_MIME_map[".woff"] = "application/x-font-woff";
+		m_MIME_map[".xhtml"] = "application/xhtml+xml";
+		m_MIME_map[".xls"] = "application/vnd.ms-excel";
+		m_MIME_map[".xml"] = "application/xml";
+		m_MIME_map[".xul"] = "application/vnd.mozilla.xul+xml";
+		m_MIME_map[".zip"] = "application/zip";
+		m_MIME_map[".3gp"] = "video/3gpp audio/3gpp";
+		m_MIME_map[".3g2"] = "video/3gpp2 audio/3gpp2";
+		m_MIME_map[".7z"] = "application/x-7z-compressed";
 	}
 
 private:
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,6 +2,7 @@
 
 // bool	g_live; // REVIEW: g_live 를 쓰길래, 전역변수 느낌인 것같아서 여기에 선언함.
 std::map<int, std::string> Response::m_status_map; // NOTE static 변수도 전역변수라도 한번 선언을 해줘야함.
+std::map<std::string, std::string> Response::m_MIME_map; // NOTE static 변수도 전역변수라도 한번 선언을 해줘야함.
 
 char** g_env;
 
@@ -10,6 +11,7 @@ int		main(int argc, char* argv[], char* envp[])
 	g_env = envp; // FIXME env 임시로 전역변수로
 
 	Response::init_status_map();
+	Response::init_MIME_map();
 	ServerManager	manager;
 	// manager.openLog();
 	if (argc > 2)
