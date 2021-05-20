5f6e3bf87fb3eda64310c3d12b6c7506aa0d9acc
yunslee <yunslee@student.42seoul.kr>
2021-05-20 17:27:59 +0900

    Fix: Response 생성자에 int status 인자값은 항상 올바른 값이 들어간다.
    Response는 생성자를 호출하는 때가 동적할당 하는 때이다.
    Server클래스의 특정 함수에서 대게 Response생성자를 호출하는데, 넣어주기전에 처리를 해주기 때문에 생성자에서는 항상 map_statuscode에서 가지고 있는 올바른 status_code 값이 들어간다.

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -15,13 +15,7 @@ Response::Response(Connection* connection, int status_code, std::string body)
 {
 	// init_status_map(); 초기화는 메인 문에서 한번 해주고 있음. 안해줘도 됨
 	this->set_m_connection(connection);
-	this->set_m_status_code(status_code);
-	if (this->m_status_map.find(status_code) != this->m_status_map.end())
-		this->set_m_status_description(this->m_status_map[status_code]);
-	else
-	{
-		std::cout << "There is no status " << status_code << " code" << std::endl; // FIXME try catch로 throw를 던져야할 것 같음.
-	}
+	this->set_m_status_code(status_code); // NOTE status_code 인자값이 잘못된 인자 값이 들어오진 않는다 Server클래스의 함수를 통해 생성되기 때문에!
 	this->make_m_firstline();	
 	this->set_m_transfer_type(GENERAL); // REVIEW chunked 일때, 값을 넣어줘야함.
 	this->set_m_body(body);
