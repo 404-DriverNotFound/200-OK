bf9e5f5cc5ecc3c81fa90929ba9b88044e5dd37f
Ykoh <ykoh@student.42seoul.kr>
2021-05-31 02:05:24 +0900

    Feat: 411 테스트 추가

--- ft_tester/statusCodeTester.cpp
+++ ft_tester/statusCodeTester.cpp
@@ -3,6 +3,75 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+void	test411(const struct sockaddr_in& sockAddr)
+{
+	{
+		int	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+		if (connect(clientSocket, (struct sockaddr*)&sockAddr, sizeof(sockAddr)) < 0)
+		{
+			throw std::exception();
+		}
+		std::cout << "TEST 411 STATUS CODE" << std::endl;
+		std::cout << "When existing content-length" << std::endl;
+		std::cout << "--------------------------" << std::endl;
+		std::string	message;
+		std::string	method("PUT");
+		std::string	uri("/put_test/temp");
+		std::string	version("HTTP/1.1");
+		message += method + " " + uri + " " + version + "\r\n";
+		message += "content-length: 10\r\n";
+		message += "\r\n";
+		message += "kkkkkkkkkk";
+		std::cout << message << std::endl;
+		std::cout << "--------------------------" << std::endl;
+		write(clientSocket, message.c_str(), message.length());
+
+		char	buf[13];
+		ssize_t	ret = read(clientSocket, buf, 13);
+		std::string	returnStatusCode = std::string(buf).substr(9, 3);
+		std::cout << "expected 200" << " returned " << returnStatusCode << std::endl;
+		std::cout << "--------------------------" << std::endl;
+		if (returnStatusCode.compare("200") != 0)
+		{
+			throw std::exception();
+		}
+		close(clientSocket);
+	}
+	std::cout << std::endl;
+	{
+		int	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+		if (connect(clientSocket, (struct sockaddr*)&sockAddr, sizeof(sockAddr)) < 0)
+		{
+			throw std::exception();
+		}
+		std::cout << "TEST 411 STATUS CODE" << std::endl;
+		std::cout << "When not existing content-length" << std::endl;
+		std::cout << "--------------------------" << std::endl;
+		std::string	message;
+		std::string	method("PUT");
+		std::string	uri("/put_test/414test2");
+		std::string	version("HTTP/1.1");
+		message += method + " " + uri + " " + version + "\r\n";
+		message += "\r\n";
+		message += "kkkkkkkkkk";
+		std::cout << message << std::endl;
+		std::cout << "--------------------------" << std::endl;
+		write(clientSocket, message.c_str(), message.length());
+
+		char	buf[13];
+		ssize_t	ret = read(clientSocket, buf, 13);
+		std::string	returnStatusCode = std::string(buf).substr(9, 3);
+		std::cout << "expected 505" << " returned " << returnStatusCode << std::endl;
+		std::cout << "--------------------------" << std::endl;
+		if (returnStatusCode.compare("505") != 0)
+		{
+			throw std::exception();
+		}
+		close(clientSocket);
+	}
+	std::cout << std::endl;
+}
+
 void	test505(const struct sockaddr_in& sockAddr)
 {
 	{
@@ -165,7 +234,7 @@ int	main(int argc, char* argv[])
 		//test400();
 		test414(sockAddr);
 		test505(sockAddr);
-		//test411();
+		test411(sockAddr);
 		//test413();
 		//test301();
 		//test404();
