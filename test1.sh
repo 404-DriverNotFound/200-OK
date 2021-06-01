ef1df21dd5df448d6bb132d981cb0007e45e6d81
yunslee <yunslee@student.42seoul.kr>
2021-06-01 13:31:16 +0900

    Fix gitAcation shell script 수정
    shell script 문법 오류 때문에 발생한 예상치 못한 동작을 수정함

--- test1.sh
+++ test1.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-./ft_tester clang++ statusCodeTester.cpp
-./ft_tester a.out 127.0.0.1:8000
+cd ./ft_tester
+clang++ statusCodeTester.cpp -o statusCodeTester
+./statusCodeTester 127.0.0.1:8000
 if [ "$?" -ne "0" ]; then
   echo "statusCodeTester"
-  exit(1);
+  exit 1
 fi
--- test2.sh
+++ test2.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
-./ft_tester clang++ ./ft_tester/byte_generator.cpp
-echo 0 | ./ft_tester/a.out
+cd ./yunslee_debugfile
+clang++ byte_generator.cpp
+echo 0 | ./a.out
 curl localhost:8000/yunslee_debugfile/1MB.bla
 if [ "$?" -ne "0" ]; then
   echo "GET 1MB.bla"
-  exit(1);
+  exit 1
 fi
 
-echo 1 | ./ft_tester/a.out
+echo 1 | ./a.out
 curl localhost:8000/yunslee_debugfile/100MB.bla
 if [ "$?" -ne "0" ]; then
   echo "GET 100MB.bla"
-  exit(1);
+  exit 1
 fi
 
-echo 2 | ./ft_tester/a.out
+echo 2 | ./a.out
 curl localhost:8000/yunslee_debugfile/1GB.bla
 if [ "$?" -ne "0" ]; then
   echo "GET 1GB.bla"
-  exit(1);
+  exit 1
 fi
