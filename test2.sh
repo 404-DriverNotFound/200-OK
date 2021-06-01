8bdf2d07303588eedeb5b5a93a91538934fb3de1
yunslee <yunslee@student.42seoul.kr>
2021-06-01 13:46:42 +0900

    Temp3
    Retry

--- test2.sh
+++ test2.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 cd ./yunslee_debugfile
-clang++ byte_generator.cpp
-# echo 0 | ./a.out
-# curl localhost:8000/yunslee_debugfile/1MB.bla
-# if [ "$?" -ne "0" ]; then
-#   echo "GET 1MB.bla"
-#   exit 1
-# fi
+echo 0 | ./ByteGenerator
+curl localhost:8000/yunslee_debugfile/1MB.bla
+if [ "$?" -ne "0" ]; then
+  echo "GET 1MB.bla"
+  exit 1
+fi
 
-# echo 1 | ./a.out
-# curl localhost:8000/yunslee_debugfile/100MB.bla
-# if [ "$?" -ne "0" ]; then
-#   echo "GET 100MB.bla"
-#   exit 1
-# fi
+echo 1 | ./ByteGenerator
+curl localhost:8000/yunslee_debugfile/100MB.bla
+if [ "$?" -ne "0" ]; then
+  echo "GET 100MB.bla"
+  exit 1
+fi
 
-# echo 2 | ./a.out
-# curl localhost:8000/yunslee_debugfile/1GB.bla
-# if [ "$?" -ne "0" ]; then
-#   echo "GET 1GB.bla"
-#   exit 1
-# fi
+echo 2 | ./ByteGenerator
+curl localhost:8000/yunslee_debugfile/1GB.bla
+if [ "$?" -ne "0" ]; then
+  echo "GET 1GB.bla"
+  exit 1
+fi
