4ca8b2210b79caae7e59c7855a83342b77ee1b0e
Ykoh <ykoh@student.42seoul.kr>
2021-06-03 18:04:29 +0900

    Chore: php CGI 보여줄 수 있는 부분 추가

---
+++ cgi-bin/game.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ko">
+  <head>
+  <meta charset="utf-8">
+    <title>숫자 맞추기 게임</title>
+    <style>
+      * {
+        font-size: 16px;
+        font-family: Consolas, sans-serif;
+      }
+    </style>
+  </head>
+  <body>
+	  <h1> 숫자맞추기 게임 </h1>
+    <form method="POST" action="cgi-bin/test.php">
+      <p><label>숫자 : <input type="text" name="number"></label>
+		<input type="submit" value="Submit"></p>
+    </form>
+  </body>
+</html>
